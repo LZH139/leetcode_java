9acc8e92fab58f43db3580817d554e6803b8f2d2
LZH139 <1391546705@qq.com>
2020-02-09 23:01:49 +0800

    LongestCommonPrefix

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -6,23 +6,38 @@ public class LongestCommonPrefix {
 
     public void out(){
         Solution s = new Solution();
-        String[] strs = new String[]{"flower","flow","flight"};
+        String[] strs = new String[]{"aa","a"};
         System.out.println(s.longestCommonPrefix(strs));
     }
 
     class Solution {
         public String longestCommonPrefix(String[] strs) {
-            char tempStr;
-            int i = 0;
-            for(String str: strs){
-                tempStr = str.charAt(i);
-
+            String result = "";
+            for(int i=0;i<strs.length;i++){
+                if(i==0){
+                    result = strs[0];
+                }else{
+                    result = longestCommonPrefix(result, strs[i]);
+                }
             }
 
-            return "ww";
+            return result;
         }
 
+        public String longestCommonPrefix(String str1, String str2){
+            String minstr = str1.length() < str2.length() ? str1 : str2;
+            for(int i=0;i<minstr.length();i++){
+                if(!isSame(str1.charAt(i), str2.charAt(i))){
+                    return str1.substring(0, i);
+                }
 
+            }
+            return minstr;
+        }
+
+        public boolean isSame(char a, char b){
+            return a==b;
+        }
 
     }
 }
