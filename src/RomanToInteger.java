bd100af9a625d36a166e6bdf91dd85035b09a8cb
LZH139 <1391546705@qq.com>
2020-02-10 11:48:20 +0800

    RomanToInteger

--- src/RomanToInteger.java
+++ src/RomanToInteger.java
@@ -6,15 +6,82 @@ public class RomanToInteger {
 
     public void out(){
         Solution s = new Solution();
-        String str = "III";
+        String str = "MCMXCIV";
         System.out.println(s.romanToInt(str));
     }
 
     class Solution {
         public int romanToInt(String s) {
             int result = 0;
-            for(int i=0; i<s.length(); i++){
+            char temp;
+            int strLength = s.length();
+            for(int i=0; i<strLength; i++){
+                temp = s.charAt(i);
+                switch (temp){
+                    case 'I':
+                        if(i+1 < strLength){
+                            if(s.charAt(i+1) == 'V'){
+                                result = result + 4;
+                                i++;
+                                break;
+                            }
+                            if(s.charAt(i+1) == 'X'){
+                                result = result + 9;
+                                i++;
+                                break;
+                            }
 
+                        }
+                        result = result + 1;
+                        break;
+                    case 'V':
+                        result = result + 5;
+                        break;
+                    case 'X':
+                        if(i+1 < strLength){
+                            if(s.charAt(i+1) == 'L'){
+                                result = result + 40;
+                                i++;
+                                break;
+                            }
+                            if(s.charAt(i+1) == 'C'){
+                                result = result + 90;
+                                i++;
+                                break;
+                            }
+
+                        }
+                        result = result + 10;
+                        break;
+                    case 'L':
+                        result = result + 50;
+                        break;
+                    case 'C':
+                        if(i+1 < strLength){
+                            if(s.charAt(i+1) == 'D'){
+                                result = result + 400;
+                                i++;
+                                break;
+                            }
+                            if(s.charAt(i+1) == 'M'){
+                                result = result + 900;
+                                i++;
+                                break;
+                            }
+
+                        }
+                        result = result + 100;
+                        break;
+                    case 'D':
+                        result = result + 500;
+                        break;
+                    case 'M':
+                        result = result + 1000;
+                        break;
+                    default:
+                        break;
+
+                }
             }
             return result;
         }
