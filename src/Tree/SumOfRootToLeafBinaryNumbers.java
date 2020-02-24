738c68b511f9494fc3728d08fa17536af3cc98af
LZH139 <1391546705@qq.com>
2020-02-24 16:35:07 +0800

    update

--- src/Tree/SumOfRootToLeafBinaryNumbers.java
+++ src/Tree/SumOfRootToLeafBinaryNumbers.java
@@ -11,14 +11,28 @@ public class SumOfRootToLeafBinaryNumbers {
 
     public void out(){
         Solution s = new Solution();
-        Integer[] tree = new Integer[]{5,2,13};
+        Integer[] tree = new Integer[]{1,0,1,0,1,0,1};
         TreeNode treehead = Tree.createTree(tree);
         System.out.println(s.sumRootToLeaf(treehead));
     }
 
     class Solution {
+        public int sum = 0;
         public int sumRootToLeaf(TreeNode root) {
-
+            helper(root,0);
+            return sum;
+        }
+        public void helper(TreeNode root, int su){
+            if(root==null){
+                return;
+            }
+            int s = root.val+su*2;
+            if(root.left == null && root.right == null) {
+                sum = sum+s;
+                return;
+            }
+            helper(root.left,s);
+            helper(root.right,s);
         }
     }
 
